648547a2c00e89ac0808b441639ee7bab022faab
Xiaoyun <yangx232@gmail.com>
2024-02-19 02:19:45 -0500

    create desc section for base page

--- src/app/shared/BasePage.tsx
+++ src/app/shared/BasePage.tsx
@@ -1,15 +1,34 @@
-import { Box, Center, Container, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Text, VStack } from "@chakra-ui/react";
 
 // `app/page.tsx` is the UI for the `/` URL
 
-export const BasePage = ({ children, title }) => {
+export const BasePage = ({
+	children,
+	title,
+	description,
+}: {
+	children: React.ReactNode;
+	title: string;
+	description?: string;
+}) => {
 	return (
 		<Container mt={20}>
-			<Center>
-				<Text as="b" fontSize="1.5rem">
-					{title}
-				</Text>
-			</Center>
+			<VStack>
+				<Container>
+					<Center>
+						<Text as="b" fontSize="1.5rem">
+							{title}
+						</Text>
+					</Center>
+				</Container>
+				{description && (
+					<Container>
+						<Center>
+							<Text fontSize="1rem">{description}</Text>
+						</Center>
+					</Container>
+				)}
+			</VStack>
 			<Box h={10} />
 			{children}
 		</Container>
--- src/app/web5/todolist/page.tsx
+++ src/app/web5/todolist/page.tsx
@@ -3,9 +3,11 @@ import { BasePage } from "@/app/shared/BasePage";
 
 // `app/page.tsx` is the UI for the `/` URL
 
+const PAGE_DESCRIPTION = "web5 todo list with browser local storage DWN";
+
 export default function Page() {
 	return (
-		<BasePage title="Web5 Todo List">
+		<BasePage title="Web5 Todo List" description={PAGE_DESCRIPTION}>
 			<TodoList />
 		</BasePage>
 	);
